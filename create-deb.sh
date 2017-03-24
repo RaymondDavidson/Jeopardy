e68409355b889b73ad98e956eb65bbf3878926e3
Rik Goldman <rikgoldman@gmail.com>
2017-03-24 17:12:03 -0400

    create-deb needs version corrected or automated

--- create-deb.sh
+++ create-deb.sh
@@ -5,11 +5,6 @@ sudo apt update
 sudo apt install -y python-stdeb
 
 cd main
-
 python setup.py --command-packages=stdeb.command sdist_dsc
-
-cd deb_dist/academic-fair-1.0/
-
-dpkg-buildpackage -rfakeroot -uc -us
-
+cd deb_dist/cs-academic-fair-17.03.24 && dpkg-buildpackage -rfakeroot -uc -us
 cd ../../
