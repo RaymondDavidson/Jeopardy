177256051cd2f3da9197c38aed0619ef9fa373ba
Rik Goldman <rikgoldman@gmail.com>
2017-03-28 12:31:18 -0400

    cleaned some files

--- build-nuitka.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash -ex
-sudo apt update
-sudo apt install python-setuptools nuitka
-# Builds with Nuitka a WIndows distribution (exe) of the code
-nuitka --standalone ./main/jeopardy-tk.py --output-dir ./nuitka/
-echo "Windows package is in $(pwd)/nuitka/"
