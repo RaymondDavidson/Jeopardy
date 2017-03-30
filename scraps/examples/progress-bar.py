79181bb7c467130b89296e150a7eda9c5f097f22
Rik Goldman <rikgoldman@gmail.com>
2017-03-30 08:12:29 -0400

    rm examples - moved to scraps

--- scraps/examples/progress-bar.py
+++
@@ -1,8 +0,0 @@
-import Tkinter as tk
-import ttk as ttk
-root = tk.Tk()
-s = ttk.Style()
-s.theme_use('clam')
-s.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
-p = ttk.Progressbar(root, style="red.Horizontal.TProgressbar", orient="horizontal", length=600, mode="determinate", maximum=4, value=1)
-p.pack()
