8bdf9d5816c9f76db586abe85484789422cd0bfa
Rik Goldman <rikgoldman@gmail.com>
2017-03-31 09:05:25 -0400

    Moved nested functions, autopep8 aggressive

--- jeopardy_quiz/jeopardy_quiz.py
+++ jeopardy_quiz/jeopardy_quiz.py
@@ -22,7 +22,8 @@
 #
 #
 
-# the pound or hash symbol means a 1 line comment follows; comments are ignored by the interpreter
+# the pound or hash symbol means a 1 line comment follows; comments are
+# ignored by the interpreter
 
 
 """
@@ -48,9 +49,259 @@ import time
 import os
 import subprocess
 
-#### Main Function ####
-def main(args=None):
+#### Functions Section ######
 
+
+def askGoogle(string):
+    """
+    In case of wrong answer, show correct 'person' from google search.
+
+    Turns correct answer into a google ask, and then opens a browser tab to the google search results.
+
+    Relies on default browser of the system to improve compatibility.
+
+    Args
+    ~~~~
+
+    :param string (str): The question to look up (from the list)
+
+
+
+    Returns
+    -------
+
+    :return bool: True for success. False otherwise.
+
+    """
+
+    ask = string
+    ask = ask.replace(" ", "+")
+    www = "https://www.google.com/#q=" + ask + "&*"
+    platform = os.name
+    if platform == "posix":
+        try:
+            webbrowser.open(www, new=0, autoraise=False)
+        except BaseException:
+            print "We're sorry; we couldn't show the answer with a browser."
+    else:
+        try:
+            webbrowser.open(www, new=0, autoraise=False)
+        except BaseException:
+            print "Couldn't raise a browser."
+
+
+def pop(title, string):
+    """
+    We used three pop-up message boxes in the course of development.
+    We don't repeat ourselves, so we just called this function each
+    time we need a messagebox to appear.
+
+    parameters
+    ----------
+
+    title (str): Title of the dialog
+    string (str): message of the messagebox
+
+    Returns
+    -------
+
+    Bool: True if successful
+
+    """
+    msg = tkMessageBox
+    msg = msg.showinfo(title, string)
+
+# function for continuing (on correct or incorrect answer)
+
+
+def out(*event):
+    """
+    Output contents to window
+
+    Output to the main window. Changes after each answer is submitted, until there are no questions left to answer.
+
+    parameters
+    ----------
+
+    event (event): Optional. How to work with <Enter> key stroke or Submit button being pressed.
+
+    """
+    global correct, ques, count, entry, further, root, browserName
+
+    # bring cursor to the input field
+    entry.focus()
+    # change progress bar
+    further["value"] = ques
+    # process input
+    ans = entry.get()
+    # conditional: if still questions left to answer, do the things that
+    # follow.
+    if ques < count:
+        entry.focus()
+        further["value"] = ques
+        further.pack()
+        # conditional: if answer is correct, increase score, change
+        # question.
+        if ans == answer[ques]:
+            correct = correct + 1
+            ques = ques + 1
+            entry.delete(0, END)
+            label.config(text=question[ques])
+            score.config(text=("Score: " + str(correct) + "/" + str(ques)))
+        # if not correct answer, do what follows.
+        else:
+            askGoogle(answer[ques])
+            #askGoogle(answer[ques], browserName)
+            root.after(0o500, lambda: root.focus_force())
+            entry.focus()
+            ques = ques + 1
+            entry.delete(0, END)
+            entry.focus()
+
+    # if there are questions left, go to next question
+    if ques < count:
+        label.config(text=question[ques])
+        score.config(text=("Score: " + str(correct) + "/" + str(ques)))
+    # if no more questions, close program.
+    else:
+        # call the close function with no parameters
+        close()
+
+
+def widgets():
+    """
+    Adds windgets to window
+
+    Function for adding all the widgets to the graphical user interface. Also sets up two frames for the lower 2/3 of frames.
+
+    Parameters:
+    None
+
+    """
+    global fontFace, further, entry, label, score, question
+
+    # frame inside window to control bottom element layout
+    bottomframe = Frame(root)
+    bottomframe.pack(side=BOTTOM)
+    # midFrame for entry
+    midFrame = Frame(root)
+    midFrame.pack(side=BOTTOM)
+
+    #### root widgets ####
+
+    dummy(root)
+
+    people = ttk.Label(
+        root,
+        wraplength=500,
+        foreground="red",
+        text="Dr. Angela Davis, Dr. Martin Luther King, Jr., Assata Shakur, bell hooks, Stokely Carmichael, Bobby Seale, Percy Sutton, Coretta Scott King, Huey P. Newton, Rep. John Lewis\n\n")
+    people.pack()
+
+    label = ttk.Label(
+        root,
+        text=question[ques],
+        wraplength=300,
+        font=fontFace)
+    label.pack()
+
+    # midFrame widgets
+    whois = StringVar(root, value='Who is...?')
+    entry = ttk.Entry(midFrame, width=40, textvariable=whois)
+    entry.focus()
+    entry.pack()
+    dummy(midFrame)
+    # Submit button: to continue game, submit answer (calls out() function)
+    submit = tk.Button(
+        midFrame,
+        text="OK",
+        command=out,
+        fg="white",
+        bg="green")
+    submit.pack()
+    # below submit button, spacers
+    dummy(midFrame)
+    dummy(midFrame)
+    dummy(midFrame)
+    # bottomframe widgets
+    # horizontal spacers (calling dummy() function defined above)
+    dummy(bottomframe)
+    # horizontal spacer: calls dummy() function above
+    dummy(bottomframe)
+
+    # current score output
+    score = ttk.Label(bottomframe, text="Score: 0/0", font=fontFace)
+    score.pack()
+    dummy(bottomframe)
+    # make and pack progressbar: (green on windows, grey on linux, striped
+    # on mac)
+    further = ttk.Progressbar(
+        bottomframe,
+        mode="determinate",
+        orient="horizontal",
+        maximum=count,
+        variable=ques,
+        length=300,
+        value=0)
+    further.pack()
+    # make label for progressbar
+    pb = ttk.Label(bottomframe, text="Progress")
+    pb.pack()
+    # spacers call the bottomframe() function
+    dummy(bottomframe)
+    dummy(bottomframe)
+
+    # quit button calls "close" function
+    leave = tk.Button(
+        bottomframe,
+        text="Quit",
+        command=close,
+        fg="white",
+        bg="red")
+    leave.pack()
+    # spacer
+    dummy(bottomframe)
+
+    # Key Binding
+    # Keyboard Input
+    root.bind('<Return>', out)
+    # Use <Escape> to quit using close method
+    root.bind('<Escape>', close)
+
+
+def close(*event):
+    """
+    close: procedure for closing app
+
+    Close the app, 1. show popup, 2. change browser contents, 3. destroy root
+
+    """
+
+    root.after(40000, lambda: root.destroy())
+    pop("Thank you", "Thank you for learning with us!")
+    webbrowser.open(
+        'https://github.com/ghoulmann/Jeopardy',
+        new=0,
+        autoraise=False)
+    root.destroy()
+
+
+def dummy(parent):
+    """ This function makes an empty label. It's used several times to add vertical space between widgets in the root window.
+
+    Parameters:
+
+    parent (variable): the variable can be set to root, midFrame, or bottomframe
+    """
+
+    spacer = ttk.Label(parent, text=" ")
+    spacer.pack()
+
+
+#### Main Function ####
+
+
+def main(args=None):
     """
     Main Function
 
@@ -63,6 +314,8 @@ def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
+    ####  Global Variables: not restricted to specific scope ####
+    global fontFace, correct, ques, count, title, fontFace, browserName, correct, ques, count, entry, further, root, browserName, question, answer
 
     #### Lists ####
 
@@ -105,9 +358,7 @@ def main(args=None):
         "She said \"this is the 21st century; we need to redefine revolution.\"\n\n",
         "He said \"Don't give up, Don't give out, Don't give in.\"\n\n",
         "She said \"Feminism is for everybody.\"\n\n",
-        "She said \"radical\" simply means \"grasping by the roots.\"\n\n"
-    ]
-
+        "She said \"radical\" simply means \"grasping by the roots.\"\n\n"]
 
     # This is a Python list.
     # Answers match the above questions (in same order) come afterwards.
@@ -144,231 +395,6 @@ def main(args=None):
     ]
     """list answers: (str)"""
 
-
-
-
-
-    ####  Global Variables: not restricted to specific scope ####
-    global fontFace, correct, ques, count, title, fontFace, browserName, correct, ques, count, entry, further, root, browserName
-
-
-    #### Functions Section ######
-
-
-    def askGoogle(string):
-        """
-        In case of wrong answer, show correct 'person' from google search.
-
-        Turns correct answer into a google ask, and then opens a browser tab to the google search results.
-
-        Relies on default browser of the system to improve compatibility.
-
-        Args
-        ~~~~
-
-        :param string (str): The question to look up (from the list)
-
-
-
-        Returns
-        -------
-
-        :return bool: True for success. False otherwise.
-
-        """
-
-        ask = string
-        ask = ask.replace(" ", "+")
-        www = "https://www.google.com/#q=" + ask + "&*"
-        platform = os.name
-        if platform == "posix":
-            try:
-                webbrowser.open(www, new=0, autoraise=False)
-            except:
-                print "We're sorry; we couldn't show the answer with a browser."
-        else:
-            try:
-                webbrowser.open(www, new=0, autoraise=False)
-            except:
-                print "Couldn't raise a browser."
-
-
-    def pop(title, string):
-
-        """
-        We used three pop-up message boxes in the course of development.
-        We don't repeat ourselves, so we just called this function each
-        time we need a messagebox to appear.
-
-        parameters
-        ----------
-
-        title (str): Title of the dialog
-        string (str): message of the messagebox
-
-        Returns
-        -------
-
-        Bool: True if successful
-
-        """
-        msg = tkMessageBox
-        msg = msg.showinfo(title, string)
-
-
-    # function for continuing (on correct or incorrect answer)
-    def out(*event):
-        """
-        Output contents to window
-
-        Output to the main window. Changes after each answer is submitted, until there are no questions left to answer.
-
-        parameters
-        ----------
-
-        event (event): Optional. How to work with <Enter> key stroke or Submit button being pressed.
-
-        """
-        global correct, ques, count, entry, further, root, browserName
-
-        # bring cursor to the input field
-        entry.focus()
-        # change progress bar
-        further["value"] = ques
-        # process input
-        ans = entry.get()
-        # conditional: if still questions left to answer, do the things that follow.
-        if ques < count:
-            entry.focus()
-            further["value"] = ques
-            further.pack()
-            # conditional: if answer is correct, increase score, change question.
-            if ans == answer[ques]:
-                correct = correct + 1
-                ques = ques + 1
-                entry.delete(0, END)
-                label.config(text = question[ques])
-                score.config(text=("Score: " + str(correct) + "/" + str(ques)))
-            # if not correct answer, do what follows.
-            else:
-                askGoogle(answer[ques])
-                #askGoogle(answer[ques], browserName)
-                root.after(0500, lambda:root.focus_force())
-                entry.focus()
-                ques = ques + 1
-                entry.delete(0, END)
-                entry.focus()
-
-        # if there are questions left, go to next question
-        if ques < count:
-            label.config(text = question[ques])
-            score.config(text=("Score: " + str(correct) + "/" + str(ques)))
-        # if no more questions, close program.
-        else:
-            # call the close function with no parameters
-            close()
-
-    def widgets():
-        """
-        Adds windgets to window
-
-        Function for adding all the widgets to the graphical user interface. Also sets up two frames for the lower 2/3 of frames.
-
-        Parameters:
-        None
-
-        """
-        global fontFace, further, entry, label, score
-
-        #frame inside window to control bottom element layout
-        bottomframe = Frame(root)
-        bottomframe.pack( side = BOTTOM )
-        # midFrame for entry
-        midFrame = Frame(root)
-        midFrame.pack(side=BOTTOM)
-
-        #### root widgets ####
-
-        dummy(root)
-
-
-        people = ttk.Label(root,wraplength=500, foreground="red", text="Dr. Angela Davis, Dr. Martin Luther King, Jr., Assata Shakur, bell hooks, Stokely Carmichael, Bobby Seale, Percy Sutton, Coretta Scott King, Huey P. Newton, Rep. John Lewis\n\n")
-        people.pack()
-
-        label = ttk.Label(root,text = question[ques],wraplength=300, font=fontFace)
-        label.pack()
-
-        # midFrame widgets
-        whois = StringVar(root, value='Who is...?')
-        entry = ttk.Entry(midFrame, width=40, textvariable=whois)
-        entry.focus()
-        entry.pack()
-        dummy(midFrame)
-        # Submit button: to continue game, submit answer (calls out() function)
-        submit = tk.Button(midFrame,text = "OK",command = out, fg="white", bg="green")
-        submit.pack()
-        #below submit button, spacers
-        dummy(midFrame)
-        dummy(midFrame)
-        dummy(midFrame)
-        # bottomframe widgets
-        #horizontal spacers (calling dummy() function defined above)
-        dummy(bottomframe)
-        # horizontal spacer: calls dummy() function above
-        dummy(bottomframe)
-
-        # current score output
-        score = ttk.Label(bottomframe, text = "Score: 0/0", font=fontFace)
-        score.pack()
-        dummy(bottomframe)
-        # make and pack progressbar: (green on windows, grey on linux, striped on mac)
-        further = ttk.Progressbar(bottomframe, mode="determinate",orient="horizontal", maximum=count, variable=ques, length=300, value=0)
-        further.pack()
-        # make label for progressbar
-        pb = ttk.Label(bottomframe, text="Progress")
-        pb.pack()
-        #spacers call the bottomframe() function
-        dummy(bottomframe)
-        dummy(bottomframe)
-
-        # quit button calls "close" function
-        leave = tk.Button(bottomframe,text = "Quit",command = close, fg="white", bg="red")
-        leave.pack()
-        #spacer
-        dummy(bottomframe)
-
-        # Key Binding
-        # Keyboard Input
-        root.bind('<Return>', out)
-        # Use <Escape> to quit using close method
-        root.bind('<Escape>', close)
-
-
-    def close(*event):
-        """
-        close: procedure for closing app
-
-        Close the app, 1. show popup, 2. change browser contents, 3. destroy root
-
-        """
-
-        root.after(40000, lambda: root.destroy())
-        pop("Thank you", "Thank you for learning with us!")
-        webbrowser.open('https://github.com/ghoulmann/Jeopardy', new=0, autoraise=False)
-        root.destroy()
-
-    def dummy(parent):
-        """ This function makes an empty label. It's used several times to add vertical space between widgets in the root window.
-
-        Parameters:
-
-        parent (variable): the variable can be set to root, midFrame, or bottomframe
-        """
-
-        spacer = ttk.Label(parent, text = " ")
-        spacer.pack()
-
-
     #### These are the first actions to happen ####
 
     # Set Variables (variables represent values that change)
@@ -398,12 +424,13 @@ def main(args=None):
     root.attributes('-topmost', True)
     # Keep the game at the forefront of the desktop
 
-
     root.mainloop()
-    # loops everything until told to quit (run out of questions or press <Escape> or click quit
+    # loops everything until told to quit (run out of questions or press
+    # <Escape> or click quit
+
 
 # Python idiom - everything runs within the main() function
 # to learn more,
-#visit <http://ibiblio.org/g2swap/byteofpython/read/module-name.html>
+# visit <http://ibiblio.org/g2swap/byteofpython/read/module-name.html>
 if __name__ == "__main__":
     main()
